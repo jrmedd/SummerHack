8337d7a9d4ec35bd8f0afde102406d1f79645971
jrmedd <hello@jamesmedd.co.uk>
2018-08-16 14:25:26 +0100

    Local timezone

--- web_app.py
+++ web_app.py
@@ -1,5 +1,7 @@
 import datetime
 
+import pytz
+
 import os
 
 from flask import Flask, render_template, request, session, redirect, jsonify, url_for, flash
@@ -9,6 +11,8 @@ from pymongo import MongoClient
 MONGO_URL = os.environ.get('MONGO_URL')
 
 CLIENT = MongoClient(MONGO_URL)
+
+MANCHESTER_TZ = pytz.timezone("Europe/London")
 
 DB = CLIENT['SummerHack']
 
@@ -26,7 +30,7 @@ TRAFFIC_CONDITIONS = ['clear', 'congestion']
 
 @APP.route('/buses/<destination>', methods=['GET'])
 def bus_time(destination):
-    time_now = int(datetime.datetime.now().strftime("%H%M"))
+    time_now = int(datetime.datetime.now(MANCHESTER_TZ).strftime("%H%M"))
     lookup = destination.title()
     results = list(BUSES.find({'destination':lookup, 'departing':{'$gte':time_now}},{'_id':0,'destination':0}).sort('departing', 1).limit(3))
     if results:
@@ -39,13 +43,13 @@ def bus_time(destination):
 
 @APP.route('/weather', methods=['GET'])
 def weather():
-    time_now = int(datetime.datetime.now().strftime("%M"))
+    time_now = int(datetime.datetime.now(MANCHESTER_TZ).strftime("%M"))
     conditions = "%s %s" % (CONDITION_ADJECTIVES[int(time_now/3)%4], WEATHER_CONDITIONS[int(time_now/15)])
     return jsonify(current_conditions=conditions)
 
 @APP.route('/traffic', methods=['GET'])
 def traffic():
-    time_now = int(datetime.datetime.now().strftime("%M"))
+    time_now = int(datetime.datetime.now(MANCHESTER_TZ).strftime("%M"))
     traffic_state = TRAFFIC_CONDITIONS[int(time_now/15)%2]
     if traffic_state != 'clear':
         traffic_state = "%s %s" % (CONDITION_ADJECTIVES[(int(time_now/3)%4)-2], traffic_state)
